74a22e860cee6064513cd1afdcc7d0f29f3c78a2
Jay-vaghani <jayvaghani10@gmail>
2022-11-19 02:25:13 +0530

    wether data fetched

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Search from './components/search/search';
 import './App.css'
 import CurrentWether from './components/current-weather/current-weather';
+import { TEMPERATURE_UNIT, WEATHER_API_KEY, WEATHER_API_URL } from './api';
+import axios from 'axios';
 
 function App() {
 
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [forecast, setForecast] = useState(null)
 
-  const handleOnSearchChange = (searchData) =>{
-    
+
+
+  const handleOnSearchChange = async (searchData) => {
+
+
+
+    const [lat, log] = await [searchData.value.lat, searchData.value.lon]
+
+    const CurrentWetherFetch = axios.get(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${log}&appid=${WEATHER_API_KEY}&${TEMPERATURE_UNIT}`).then((response) => {
+      setCurrentWeather({
+        city: searchData.label,
+        ...response.data.main
+      })
+    }).catch((error) => {
+      console.log(error)
+    })
+
+    const ForecastFetch = axios.get(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${log}&appid=${WEATHER_API_KEY}&${TEMPERATURE_UNIT}`).then((response) => {
+
+      setForecast({
+        city: searchData.label,
+        ...response.data.list
+      })
+    }).catch((error) => {
+      console.log(error)
+    })
   }
+
+  console.log(currentWeather);
+  console.log(forecast);
 
   return (
     <div className='container'>
-      <Search onSearchChange={handleOnSearchChange}/>
+      <Search onSearchChange={handleOnSearchChange} />
       <CurrentWether />
     </div>
   );
